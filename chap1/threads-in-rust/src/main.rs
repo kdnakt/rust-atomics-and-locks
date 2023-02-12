6484d65324d3fc7f8b4d301598669cfae60f05ae
kdnakt <a.kid.1985@gmail.com>
2023-02-13 02:45:31 +0900

    send back value from thread

--- chap1/threads-in-rust/src/main.rs
+++ chap1/threads-in-rust/src/main.rs
@@ -33,6 +33,15 @@ fn main() {
             println!("{n}");
         }
     }).join().unwrap();
+
+    let numbers = Vec::from_iter(0..=1000);
+    let t = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.iter().sum::<usize>();
+        sum / len
+    });
+    let average = t.join().unwrap();
+    println!("average: {average}");
 }
 
 fn f() {
