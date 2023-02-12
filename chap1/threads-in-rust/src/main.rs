0d660ce4ee875f110e0bd4694089fa81ccb56a82
kdnakt <a.kid.1985@gmail.com>
2023-02-12 22:51:18 +0900

    fix comment

--- chap1/threads-in-rust/src/main.rs
+++ chap1/threads-in-rust/src/main.rs
@@ -1,5 +1,18 @@
 use std::thread;
 
+/**
+example output:
+Hello from the main thread.
+Hello from another thread!
+This is my thread id: ThreadId(2)
+Hello from another thread!
+This is my thread id: ThreadId(3)
+
+another output:
+Hello from the main thread.
+Hello from another thread!
+This is my thread id: ThreadId(
+*/
 fn main() {
     thread::spawn(f);
     thread::spawn(f);
@@ -13,11 +26,3 @@ fn f() {
     let id = thread::current().id();
     println!("This is my thread id: {id:?}");
 }
-
-/**
-Hello from the main thread.
-Hello from another thread!
-This is my thread id: ThreadId(2)
-Hello from another thread!
-This is my thread id: ThreadId(3)
-*/
