eb8365f6248e2388e77d8125cecd34f255281bc6
kdnakt <a.kid.1985@gmail.com>
2023-02-12 22:53:41 +0900

    use the JoinHandle to wait threads

--- chap1/threads-in-rust/src/main.rs
+++ chap1/threads-in-rust/src/main.rs
@@ -8,16 +8,19 @@ This is my thread id: ThreadId(2)
 Hello from another thread!
 This is my thread id: ThreadId(3)
 
-another output:
+another output without JoinHandle:
 Hello from the main thread.
 Hello from another thread!
 This is my thread id: ThreadId(
 */
 fn main() {
-    thread::spawn(f);
-    thread::spawn(f);
+    let t1 = thread::spawn(f);
+    let t2 = thread::spawn(f);
 
     println!("Hello from the main thread.");
+
+    t1.join().unwrap();
+    t2.join().unwrap();
 }
 
 fn f() {
