f4b4e3fd8b47212cf1a505a87e2e5583e54523ed
kdnakt <a.kid.1985@gmail.com>
2023-02-15 00:07:55 +0900

    leaking an allocation

--- chap1/shared_ownership_and_reference_counting/src/main.rs
+++ chap1/shared_ownership_and_reference_counting/src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // dbg! macro will print expression and result to stderr
     thread::spawn(|| dbg!(&X));
     thread::spawn(|| dbg!(&X));
+
+    // never drop and deallocate it
+    let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
+    thread::spawn(move || dbg!(x));
+    thread::spawn(move || dbg!(x));
 }
