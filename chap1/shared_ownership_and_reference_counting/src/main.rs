3d9fcfe19728648ebd0619f0b302e6330fb56769
kdnakt <a.kid.1985@gmail.com>
2023-02-15 00:19:17 +0900

    cannot send an Rc to another thread

--- chap1/shared_ownership_and_reference_counting/src/main.rs
+++ chap1/shared_ownership_and_reference_counting/src/main.rs
@@ -1,4 +1,5 @@
 use std::thread;
+use std::rc::Rc;
 fn main() {
     static X: &'static [i32; 3] = &[1, 2, 3];
 
@@ -10,4 +11,14 @@ fn main() {
     let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
     thread::spawn(move || dbg!(x));
     thread::spawn(move || dbg!(x));
+
+    // reference counting
+    let a = Rc::new([1, 2, 3]);
+    let b = a.clone(); // increment a counter stored next to the contained value
+
+    assert_eq!(a.as_ptr(), b.as_ptr());
+    // same allocation, they share ownership!
+
+    // thread::spawn(move || dbg!(b));
+    // compile error: Rc cannot be sent between threads safely
 }
