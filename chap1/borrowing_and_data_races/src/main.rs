955b1c75d22983f435b1ed5fed589d51b83b9323
kdnakt <a.kid.1985@gmail.com>
2023-02-16 08:29:42 +0900

    unsafe method call

--- chap1/borrowing_and_data_races/src/main.rs
+++ chap1/borrowing_and_data_races/src/main.rs
@@ -2,6 +2,11 @@ fn main() {
     let x = 1;
     let mut y = 2;
     f(&x, &mut y);
+
+    let index = 3;
+    let a = [123, 456, 789];
+    let b = unsafe { a.get_unchecked(index) };
+    println!("{:?} {b}", a); // [123, 456, 789] 1829694064
 }
 
 fn f(a: &i32, b: &mut i32) {
