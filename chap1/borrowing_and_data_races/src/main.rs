2418465f1ade3459fe6afcf8fc2104d764ebbe6e
kdnakt <a.kid.1985@gmail.com>
2023-02-17 01:53:00 +0900

    undefined behavior can propagate both backwards and forwards

--- chap1/borrowing_and_data_races/src/main.rs
+++ chap1/borrowing_and_data_races/src/main.rs
@@ -3,7 +3,16 @@ fn main() {
     let mut y = 2;
     f(&x, &mut y);
 
-    let index = 3;
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf);
+    let index = buf.trim().parse::<usize>().unwrap();
+
+    match index {
+        0 => println!("index is zero"),
+        1 => println!("index is one"),
+        _ => g(index),
+    }
+
     let a = [123, 456, 789];
     let b = unsafe { a.get_unchecked(index) };
     println!("{:?} {b}", a); // [123, 456, 789] 1829694064
@@ -21,4 +30,12 @@ fn f(a: &i32, b: &mut i32) {
         panic!("never happens");
     }
     println!("end of f()");
+}
+
+fn g(index: usize) {
+    println!("g: start");
+    if index == 3 {
+        println!("g: index = 3");
+    }
+    println!("g: end");
 }
