b28ac66840b89838f4f8143cc33c2e7abf22c5ce
kdnakt <a.kid.1985@gmail.com>
2023-02-21 00:53:29 +0900

    Mutex in practice

---
+++ chap1/locking_mutexes_and_rwlocks/src/main.rs
@@ -0,0 +1,21 @@
+use std::sync::Mutex;
+use std::thread;
+
+fn main() {
+    println!("Hello, world!");
+
+    let n = Mutex::new(0);
+    thread::scope(|s| {
+        for _ in 0..10 {
+            s.spawn(|| {
+                let mut guard = n.lock().unwrap();
+                for _ in 0..100 {
+                    *guard += 1;
+                }
+            });
+        }
+    });
+    let unwrapped = n.into_inner().unwrap();
+    println!("{}", unwrapped);
+    assert_eq!(unwrapped, 1000);
+}
