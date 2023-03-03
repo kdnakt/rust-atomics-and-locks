3fee5b4352d53a041553dd27290c8d6d5eac8324
kdnakt <a.kid.1985@gmail.com>
2023-03-03 22:57:47 +0900

    example progress reporting

---
+++ chap2/progress_reporting/src/main.rs
@@ -0,0 +1,28 @@
+use std::sync::atomic::AtomicUsize;
+use std::thread;
+use std::time::Duration;
+use std::sync::atomic::Ordering::Relaxed;
+
+fn main() {
+    println!("Hello, world!");
+    
+    let num_done = AtomicUsize::new(0);
+    thread::scope(|s| {
+        // background thread to process all 100 items
+        s.spawn(|| {
+            for i in 0..100 {
+                thread::sleep(Duration::from_millis(i));
+                num_done.store((i + 1).try_into().unwrap(), Relaxed);
+            }
+        });
+        // main thread shows status updates every second
+        loop {
+            let n = num_done.load(Relaxed);
+            if n == 100 { break; }
+            println!("Working.. {n}/100 done");
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    println!("Done!");
+}
