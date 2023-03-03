42bcaee362d9d3a20bc44b3cf9c03a5d578fc6bd
kdnakt <a.kid.1985@gmail.com>
2023-03-04 00:08:28 +0900

    notify every time with thread::unpark()

--- chap2/progress_reporting/src/main.rs
+++ chap2/progress_reporting/src/main.rs
@@ -7,12 +7,17 @@ fn main() {
     println!("Hello, world!");
     
     let num_done = AtomicUsize::new(0);
+
+    let main_thread = thread::current();
+
     thread::scope(|s| {
         // background thread to process all 100 items
         s.spawn(|| {
             for i in 0..100 {
                 thread::sleep(Duration::from_millis(i));
                 num_done.store((i + 1).try_into().unwrap(), Relaxed);
+                // wake up the main thread
+                main_thread.unpark();
             }
         });
         // main thread shows status updates every second
@@ -20,7 +25,8 @@ fn main() {
             let n = num_done.load(Relaxed);
             if n == 100 { break; }
             println!("Working.. {n}/100 done");
-            thread::sleep(Duration::from_secs(1));
+            // thread::sleep(Duration::from_secs(1));
+            thread::park_timeout(Duration::from_secs(1));
         }
     });
 
