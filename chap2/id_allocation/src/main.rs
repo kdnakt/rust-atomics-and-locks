c11a33595569be988f47ea9ef2d43048ec0d0667
kdnakt <a.kid.1985@gmail.com>
2023-03-10 23:52:54 +0900

    use compare_exchange_weak()

--- chap2/id_allocation/src/main.rs
+++ chap2/id_allocation/src/main.rs
@@ -7,26 +7,41 @@ use std::thread;
 static NEXT_ID: AtomicU32 = AtomicU32::new(0);
 
 fn allocate_new_id() -> u32 {
-    let id = NEXT_ID.fetch_add(1, Relaxed);
-    // problematic
-    // assert!(id < 1000, "too many IDs!");
-    if id >= 1000 {
-        NEXT_ID.fetch_sub(1, Relaxed);
-        panic!("too many IDs!");
+    // let id = NEXT_ID.fetch_add(1, Relaxed);
+    // // problematic
+    // // assert!(id < 1000, "too many IDs!");
+    // if id >= 1000 {
+    //     NEXT_ID.fetch_sub(1, Relaxed);
+    //     panic!("too many IDs!");
+    // }
+    // id
+
+    // use compare and exchange
+    let mut id = NEXT_ID.load(Relaxed);
+    loop {
+        assert!(id < 1000, "too many IDs!");
+        match NEXT_ID.compare_exchange_weak(id, id + 1, Relaxed, Relaxed) {
+            Ok(_) => return id,
+            Err(v) => {
+                println!("expected={id}, got={v}");
+                id = v;
+            },
+        }
     }
-    id
 }
 
 fn main() {
     println!("Hello, world!");
 
     thread::scope(|s| {
-        for i in 0..10 {
+        for i in 0..100 {
             s.spawn(move || {
                 let id = allocate_new_id();
-                println!("thread {i} gets id {id}");
+                // println!("thread {i} gets id {id}");
             });
         }
     });
+
+    println!("Done!");
 }
 
