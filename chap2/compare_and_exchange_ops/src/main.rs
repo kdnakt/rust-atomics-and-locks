021cec46b34f8d991e585ff6863d14d0d3098663
kdnakt <a.kid.1985@gmail.com>
2023-03-10 22:42:41 +0900

    compare and exchange

---
+++ chap2/compare_and_exchange_ops/src/main.rs
@@ -0,0 +1,28 @@
+use std::sync::atomic::AtomicU32;
+use std::sync::atomic::Ordering::Relaxed;
+use std::thread;
+
+fn increment(a: &AtomicU32) {
+    let mut current = a.load(Relaxed);
+    loop {
+        let new = current + 1;
+        match a.compare_exchange(current, new, Relaxed, Relaxed) {
+            Ok(_) => return,
+            Err(v) => current = v,
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let a = &AtomicU32::new(0);
+    thread::scope(|s| {
+        for i in 0..10 {
+            s.spawn(move || {
+                increment(a);
+                println!("{i}: a={:?}", a);
+            });
+        }
+    })
+}
