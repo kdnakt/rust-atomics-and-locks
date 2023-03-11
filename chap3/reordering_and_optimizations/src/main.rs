fd5eefb2b80ee5de41d8e14b0c1e3aec3406189e
kdnakt <a.kid.1985@gmail.com>
2023-03-12 02:37:03 +0900

    compiler and processor optimization

--- chap3/reordering_and_optimizations/src/main.rs
+++ chap3/reordering_and_optimizations/src/main.rs
@@ -3,6 +3,14 @@ fn f(a: &mut i32, b: &mut i32) {
     *a += 1;
     *b += 1;
     *a += 1;
+
+    // compiler might reorder above like this, for faster execution
+    // *a += 2;
+    // *b += 1;
+
+    // And Processor might execute the second addition before the first
+    // possibly because *a had to be fetched from the main memory
+    // and *b was available in a cache.
 }
 
 fn main() {
