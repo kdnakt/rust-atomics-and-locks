678b56999cbd33a71018e628a1d4b1ff6f076726
kdnakt <a.kid.1985@gmail.com>
2023-03-14 09:51:29 +0900

    happens before

---
+++ chap3/happens_before_relationship/src/main.rs
@@ -0,0 +1,31 @@
+use std::sync::atomic::AtomicI32;
+use std::sync::atomic::Ordering::Relaxed;
+
+use std::thread;
+
+static X: AtomicI32 = AtomicI32::new(0);
+static Y: AtomicI32 = AtomicI32::new(0);
+
+fn a() {
+    X.store(10, Relaxed);
+    Y.store(20, Relaxed);
+}
+
+fn b() {
+    let y = Y.load(Relaxed);
+    let x = X.load(Relaxed);
+    println!("x={x}, y={y}");
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            a();
+        });
+        s.spawn(|| {
+            b();
+        });
+    });
+}
