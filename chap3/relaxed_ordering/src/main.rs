af70cd24069a5d0609b56c225516d4c956336620
kdnakt <a.kid.1985@gmail.com>
2023-03-16 09:21:37 +0900

    all threads agree on a single order of modification

--- chap3/relaxed_ordering/src/main.rs
+++ chap3/relaxed_ordering/src/main.rs
@@ -5,10 +5,19 @@ use std::sync::atomic::{
 use std::thread;
 
 static X: AtomicI32 = AtomicI32::new(0);
+static Y: AtomicI32 = AtomicI32::new(0);
 
 fn a() {
     X.fetch_add(5, Relaxed);
     X.fetch_add(10, Relaxed);
+}
+
+fn a1() {
+    Y.fetch_add(5, Relaxed);
+}
+
+fn a2() {
+    Y.fetch_add(10, Relaxed);
 }
 
 fn b() {
@@ -16,7 +25,17 @@ fn b() {
     let b = X.load(Relaxed);
     let c = X.load(Relaxed);
     let d = X.load(Relaxed);
-    println!("{a} {b} {c} {d}");
+    println!("X: {a} {b} {c} {d}");
+    // threads can't observe the result inconsistent with total modification order, defined in a()
+    // 0 0 5 15 is possible but 0 5 0 15 is impossible
+}
+
+fn b2() {
+    let a = Y.load(Relaxed);
+    let b = Y.load(Relaxed);
+    let c = Y.load(Relaxed);
+    let d = Y.load(Relaxed);
+    println!("Y: {a} {b} {c} {d}");
     // threads can't observe the result inconsistent with total modification order, defined in a()
     // 0 0 5 15 is possible but 0 5 0 15 is impossible
 }
@@ -30,6 +49,18 @@ fn main() {
         });
         s.spawn(|| {
             b();
+        });
+    });
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            a1();
+        });
+        s.spawn(|| {
+            a2();
+        });
+        s.spawn(|| {
+            b2();
         });
     });
 }
