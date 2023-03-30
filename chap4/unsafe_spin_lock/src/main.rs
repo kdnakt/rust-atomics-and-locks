71f8547ec63b497f755ca248936922831d3807d9
kdnakt <a.kid.1985@gmail.com>
2023-03-30 10:10:58 +0900

    implement Sync for SpinLock

--- chap4/unsafe_spin_lock/src/main.rs
+++ chap4/unsafe_spin_lock/src/main.rs
@@ -5,7 +5,22 @@ use std::sync::atomic::{
 
 pub struct SpinLock<T> {
     locked: AtomicBool,
+    // UnsafeCel doesn't implement Sync: not shareable btw threads
     value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for SpinLock<T>
+    // only allow one thread at a time to access the T
+    // so no need T to implement Sync
+    where T: Send {}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
 }
 
 fn main() {
