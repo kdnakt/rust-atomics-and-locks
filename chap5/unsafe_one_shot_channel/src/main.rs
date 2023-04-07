b1df86a3a5c0daf893941eae77b482515fad423a
kdnakt <a.kid.1985@gmail.com>
2023-04-07 09:47:51 +0900

    start with struct definition

---
+++ chap5/unsafe_one_shot_channel/src/main.rs
@@ -0,0 +1,13 @@
+// unsafe version of Option<T>: requires its user to manually keep track of whether it is initialized
+use std::mem::MaybeUninit;
+use std::cell::UnsafeCell;
+use std::sync::atomic::AtomicBool;
+
+pub struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
