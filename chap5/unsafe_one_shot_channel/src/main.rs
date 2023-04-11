ddf4442c89325f9063777928b2114dcee94a0c25
kdnakt <a.kid.1985@gmail.com>
2023-04-11 16:52:45 +0900

    make the receive method validate the ready flag

--- chap5/unsafe_one_shot_channel/src/main.rs
+++ chap5/unsafe_one_shot_channel/src/main.rs
@@ -38,9 +38,15 @@ impl <T> Channel<T> {
         self.ready.load(Acquire)
     }
 
-    // Safety: Only call this once
-    // and only after is_ready() returns true!
+    /// Panics if no message is available yet.
+    ///
+    /// Tip: Use `is_ready` to check first.
+    ///
+    /// Safety: Only call this once
     pub unsafe fn receive(&self) -> T {
+        if !self.ready.load(Acquire) {
+            panic!("no message available!");
+        }
         (*self.message.get()).assume_init_read()
     }
 }
