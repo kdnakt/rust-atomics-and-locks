8f4843cb1afdfbe88f03c31e4f53c1cf39e3a8c2
kdnakt <a.kid.1985@gmail.com>
2023-04-18 00:36:27 +0900

    create a sender-receiver pair

--- chap5/safety_through_types/src/main.rs
+++ chap5/safety_through_types/src/main.rs
@@ -17,10 +17,19 @@ pub struct Receiver<T> {
 struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
     ready: AtomicBool,
+    // in_use is no longer needed since type system guarantees that
 }
 
 unsafe impl<T> Sync for Channel<T>
     where T: Send {}
+
+pub fn channel<T> -> (Sender<T>, Receiver<T>) {
+    let a = Arc::new(Channel {
+        message: UnsafeCell::new(MaybeUninit::uninit()).
+        ready:: AtomicBool::new(false),
+    });
+    (Sender { channel: a.clone() }, Receiver { channel: a })
+}
 
 fn main() {
     println!("Hello, world!");
