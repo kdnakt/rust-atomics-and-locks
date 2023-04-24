7d9982aef9293832749e4d389586e3ecf4a50b27
kdnakt <a.kid.1985@gmail.com>
2023-04-24 10:13:32 +0900

    implement send() and receive() with lifetime

--- chap5/borrowing_to_avoid_allocation/src/main.rs
+++ chap5/borrowing_to_avoid_allocation/src/main.rs
@@ -1,4 +1,11 @@
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{
+    AtomicBool,
+    Ordering::{
+        Acquire,
+        Relaxed,
+        Release,
+    },
+};
 use std::cell::UnsafeCell;
 use std::mem::MaybeUninit;
 
@@ -24,12 +31,40 @@ impl<T> Channel<T> {
 unsafe impl<T> Sync for Channel<T>
     where T: Send {}
 
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
+}
+
 pub struct Sender<'a, T> {
     channel: &'a Channel<T>,
 }
 
+impl<T> Sender<'_, T> {
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Release);
+    }
+}
+
 pub struct Receiver<'a, T> {
     channel: &'a Channel<T>,
+}
+
+impl<T> Receiver<'_, T> {
+    pub fn is_ready(&self) -> bool {
+        self.channel.ready.load(Relaxed)
+    }
+
+    pub fn receive(self) -> T {
+        if !self.channel.ready.swap(false, Acquire) {
+            panic!("no message available!");
+        }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
+    }
 }
 
 fn main() {
