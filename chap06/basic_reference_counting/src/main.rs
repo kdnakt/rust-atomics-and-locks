6cb9514b227ffe14b5ad9ca1f98ce4d2aa9674a2
kdnakt <a.kid.1985@gmail.com>
2023-04-29 08:49:26 +0900

    Arc with std::ptr::NonNull

--- chap06/basic_reference_counting/src/main.rs
+++ chap06/basic_reference_counting/src/main.rs
@@ -1,8 +1,15 @@
 use std::sync::atomic::AtomicUsize;
+use std::ptr::NonNull;
 
 struct ArcData<T> {
     ref_count: AtomicUsize,
     data: T,
+}
+
+// Not a Box, which is exclusive ownership.
+// We use pointer for shared ownership.
+pub struct Arc<T> {
+    ptr: NonNull<ArcData<T>>,
 }
 
 fn main() {
