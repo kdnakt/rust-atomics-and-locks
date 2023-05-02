af9f75fff0838abec2f465170b6d0e32b48936dd
kdnakt <a.kid.1985@gmail.com>
2023-05-02 09:04:39 +0900

    the clone implementation for our Arc

--- chap06/basic_reference_counting/src/main.rs
+++ chap06/basic_reference_counting/src/main.rs
@@ -1,6 +1,7 @@
 use std::sync::atomic::AtomicUsize;
 use std::ptr::NonNull;
 use std::ops::Deref;
+use std::sync::atomic::Ordering::Relaxed;
 
 struct ArcData<T> {
     ref_count: AtomicUsize,
@@ -42,6 +43,16 @@ impl<T> Deref for Arc<T> {
 
     fn deref(&self) -> &T {
         &self.data().data
+    }
+}
+
+impl<T> Clone for Arc<T> {
+    fn clone(&self) -> Self {
+        // TODO: handle overflows
+        self.data().ref_count.fetch_add(1, Relaxed);
+        Arc {
+            ptr: self.ptr,
+        }
     }
 }
 
