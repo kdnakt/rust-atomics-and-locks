684b80fc2de1d52f018e7ebc3cf9cca4adbccfbd
kdnakt <a.kid.1985@gmail.com>
2023-05-07 17:52:20 +0900

    Arc contains a Weak

--- chap06/weak_pointers/src/main.rs
+++ chap06/weak_pointers/src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::atomic::AtomicUsize;
 use std::cell::UnsafeCell;
+use std::ptr::NonNull;
 
 struct ArcData<T> {
     /// number of `Arc`s
@@ -9,6 +10,17 @@ struct ArcData<T> {
     /// the data. `None` if there's only weak pointers left.
     data: UnsafeCell<Option<T>>,
 }
+
+pub struct Arc<T> {
+    weak: Weak<T>,
+}
+
+pub struct Weak<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Sync + Send> Send for Weak<T> {}
+unsafe impl<T: Sync + Send> Sync for Weak<T> {}
 
 fn main() {
     println!("Hello, world!");
