40fe73f9116ec2d666f9e72c0960b04b1d569080
kdnakt <a.kid.1985@gmail.com>
2023-05-13 09:02:48 +0900

    upgrade() method on Weak<T>

--- chap06/optimizing/src/main.rs
+++ chap06/optimizing/src/main.rs
@@ -89,6 +89,24 @@ impl<T> Weak<T> {
     fn data(&self) -> &ArcData<T> {
         unsafe { self.ptr.as_ref() }
     }
+
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        let mut n = self.data().data_ref_count.load(Relaxed);
+        loop {
+            if n == 0 {
+                return None;
+            }
+            assert!(n <= usize::MAX / 2);
+            if let Err(e) = self.data()
+                .data_ref_count
+                .compare_exchange_weak(n, n + 1, Relaxed, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Some(Arc { ptr: self.ptr });
+        }
+    }
 }
 
 impl<T> Clone for Weak<T> {
