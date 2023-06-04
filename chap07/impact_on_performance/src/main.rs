25678d65e2f69fad38f8ab62575dbd044e1d67a4
kdnakt <a.kid.1985@gmail.com>
2023-06-04 17:36:27 +0900

    avoid compiler optimizations

--- chap07/impact_on_performance/src/main.rs
+++ chap07/impact_on_performance/src/main.rs
@@ -1,14 +1,16 @@
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering::Relaxed;
 use std::time::Instant;
+use std::hint::black_box;
 
 static A: AtomicU64 = AtomicU64::new(0);
 
 fn main() {
     println!("Hello, world!");
+    black_box(&A);
     let start = Instant::now();
     for _ in 0..1_000_000_000 {
-        A.load(Relaxed);
+        black_box(A.load(Relaxed));
     }
     println!("{:?}", start.elapsed());
 }
