56c64d3c670d349c3eb9583b3c9f9bf572f24f74
kdnakt <a.kid.1985@gmail.com>
2023-06-09 00:49:38 +0900

    try background thread

--- chap07/impact_on_performance/src/main.rs
+++ chap07/impact_on_performance/src/main.rs
@@ -2,12 +2,20 @@ use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering::Relaxed;
 use std::time::Instant;
 use std::hint::black_box;
+use std::thread;
 
 static A: AtomicU64 = AtomicU64::new(0);
 
 fn main() {
     println!("Hello, world!");
     black_box(&A);
+
+    thread::spawn(|| {
+        loop {
+            black_box(A.load(Relaxed));
+        }
+    });
+
     let start = Instant::now();
     for _ in 0..1_000_000_000 {
         black_box(A.load(Relaxed));
