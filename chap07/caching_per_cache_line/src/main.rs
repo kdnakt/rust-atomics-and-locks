150462e220075ba891f7a950e9083faa786cf023
kdnakt <a.kid.1985@gmail.com>
2023-06-12 23:28:52 +0900

    spacing variables so they each get their own cache line of 64-byte

--- chap07/caching_per_cache_line/src/main.rs
+++ chap07/caching_per_cache_line/src/main.rs
@@ -1,15 +1,18 @@
 use std::sync::atomic::{
-    AtomicU64,
+    AtomicU64, // which is only 8 bytes
     Ordering::Relaxed,
 };
 use std::hint::black_box;
 use std::thread;
 use std::time::Instant;
 
-static A: [AtomicU64; 3] = [
-    AtomicU64::new(0),
-    AtomicU64::new(0),
-    AtomicU64::new(0),
+#[repr(align(64))] // This struct must be 64-byte aligned.
+struct Aligned(AtomicU64); // adds 56 bytes of padding
+
+static A: [Aligned; 3] = [
+    Aligned(AtomicU64::new(0)),
+    Aligned(AtomicU64::new(0)),
+    Aligned(AtomicU64::new(0)),
 ];
 
 fn main() {
@@ -18,14 +21,14 @@ fn main() {
     black_box(&A);
     thread::spawn(|| {
         loop {
-            A[0].store(0, Relaxed);
-            A[2].store(0, Relaxed);
+            A[0].0.store(0, Relaxed);
+            A[2].0.store(0, Relaxed);
         }
     });
 
     let start = Instant::now();
     for _ in 0..1_000_000_000 {
-        black_box(A[1].load(Relaxed));
+        black_box(A[1].0.load(Relaxed));
     }
     println!("{:?}", start.elapsed());
 }
