1e1f72274d6d59dfce62eb485106ccf4b8bd8330
kdnakt <a.kid.1985@gmail.com>
2023-06-29 09:43:53 +0900

    futex wait and wake

--- chap08/futex/src/main.rs
+++ chap08/futex/src/main.rs
@@ -1,4 +1,9 @@
-use std::sync::atomic::AtomicU32;
+use std::sync::atomic::{
+    AtomicU32,
+    Ordering::Relaxed,
+};
+use std::thread;
+use std::time::Duration;
 
 #[cfg(not(target_os = "linux"))]
 compile_error!("Linux only. Sorry!");
@@ -30,4 +35,20 @@ pub fn wake_one(a: &AtomicU32) {
 
 fn main() {
     println!("Hello, world!");
+
+    let a = AtomicU32::new(0);
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            thread::sleep(Duration::from_secs(3));
+            a.store(1, Relaxed);
+            wake_one(&a);
+        })
+    });
+
+    println!("Waiting ...");
+    while a.load(Relaxed) == 0 {
+        wait(&a, 0);
+    }
+    println!("Done!");
 }
