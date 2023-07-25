7eff964bf1963ea90518eea1cf73d90d69d2c099
kdnakt <a.kid.1985@gmail.com>
2023-07-25 12:49:12 +0900

    add benchmarking test

--- chap09/avoiding_syscalls/src/main.rs
+++ chap09/avoiding_syscalls/src/main.rs
@@ -15,6 +15,7 @@ use atomic_wait::{
     wait,
     wake_one,
 };
+use std::time::Instant;
 
 pub struct Mutex<T> {
     /// 0: unlocked
@@ -92,4 +93,15 @@ impl<T> Drop for MutexGuard<'_, T> {
 
 fn main() {
     println!("Hello, world!");
+
+    // benchmarking
+    let m = Mutex::new(0);
+    std::hint::black_box(&m);
+
+    let start = Instant::now();
+    for _ in 0..5_000_000 {
+        *m.lock() += 1;
+    }
+    let duration = start.elapsed();
+    println!("locked {} times in {:?}", *m.lock(), duration);
 }
