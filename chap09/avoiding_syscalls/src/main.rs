b37535984f1b6d879319e1611c4d028d70ed362b
kdnakt <a.kid.1985@gmail.com>
2023-07-26 09:47:10 +0900

    scenario with 4 threads

--- chap09/avoiding_syscalls/src/main.rs
+++ chap09/avoiding_syscalls/src/main.rs
@@ -96,14 +96,22 @@ fn main() {
 
     // benchmarking:
     // Debug mode on M1 MacBook Pro, 119.274708ms
+    //     with 4 threads, 1.987054833s
     // Release mode on M1 MacBook Pro, 44.6345ms
+    //     with 4 threads, 1.615139333s
     let m = Mutex::new(0);
     std::hint::black_box(&m);
 
     let start = Instant::now();
-    for _ in 0..5_000_000 {
-        *m.lock() += 1;
-    }
+    std::thread::scope(|s| {
+        for _ in 0..4 {
+            s.spawn(|| {
+                for _ in 0..5_000_000 {
+                    *m.lock() += 1;
+                }
+            });
+        }
+    });
     let duration = start.elapsed();
     println!("locked {} times in {:?}", *m.lock(), duration);
 }
