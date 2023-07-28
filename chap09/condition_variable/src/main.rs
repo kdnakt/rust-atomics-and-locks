0a113919f37650095847f05b70b8c34060264095
kdnakt <a.kid.1985@gmail.com>
2023-07-28 16:25:39 +0900

    add Condvar::notify_one()

--- chap09/condition_variable/src/main.rs
+++ chap09/condition_variable/src/main.rs
@@ -1,4 +1,8 @@
-use std::sync::atomic::AtomicU32;
+use std::sync::atomic::{
+    AtomicU32,
+    Ordering::Relaxed,
+};
+use atomic_wait::wake_one;
 
 pub struct Condvar {
     counter: AtomicU32,
@@ -7,6 +11,11 @@ pub struct Condvar {
 impl Condvar {
     pub const fn new() -> Self {
         Self { counter: AtomicU32::new(0) }
+    }
+
+    pub fn notify_one(&self) {
+        self.counter.fetch_add(1, Relaxed);
+        wake_one(&self.counter);
     }
 }
 
