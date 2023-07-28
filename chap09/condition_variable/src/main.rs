4cffcb4254e5b3cef3049fd46f1ee2039ae590c6
kdnakt <a.kid.1985@gmail.com>
2023-07-28 15:53:36 +0900

    add Condvar::new()

--- chap09/condition_variable/src/main.rs
+++ chap09/condition_variable/src/main.rs
@@ -4,6 +4,12 @@ pub struct Condvar {
     counter: AtomicU32,
 }
 
+impl Condvar {
+    pub const fn new() -> Self {
+        Self { counter: AtomicU32::new(0) }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
