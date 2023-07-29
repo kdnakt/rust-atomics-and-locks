70d66a78f737d3ba3d313b0dd42828285b171aa8
kdnakt <a.kid.1985@gmail.com>
2023-07-29 11:24:13 +0900

    add Mutex and MutexGuard

--- chap09/condition_variable/src/main.rs
+++ chap09/condition_variable/src/main.rs
@@ -1,11 +1,96 @@
 use std::sync::atomic::{
     AtomicU32,
-    Ordering::Relaxed,
+    Ordering::{
+        Acquire,
+        Relaxed,
+        Release,
+    },
+};
+use std::cell::UnsafeCell;
+use std::ops::{
+    Deref,
+    DerefMut,
 };
 use atomic_wait::{
+    wait,
     wake_one,
     wake_all,
 };
+use std::time::Instant;
+
+pub struct Mutex<T> {
+    /// 0: unlocked
+    /// 1: locked, no other threads waiting
+    /// 2: locked, other threads waiting
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for Mutex<T>
+    where T: Send {}
+
+impl<T> Mutex<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0), // unlocked state
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> MutexGuard<T> {
+        if self.state.compare_exchange(0, 1, Acquire, Relaxed).is_err() {
+            // while self.state.swap(2, Acquire) != 0 {
+            //     wait(&self.state, 2);
+            // }
+
+            // The lock was already locked :(
+            lock_contended(&self.state);
+        }
+        MutexGuard { mutex: self }
+    }
+}
+
+fn lock_contended(state: &AtomicU32) {
+    let mut spin_count = 0;
+
+    while state.load(Relaxed) == 1 && spin_count < 100 {
+        spin_count += 1;
+        std::hint::spin_loop();
+    }
+
+    if state.compare_exchange(0, 1, Acquire, Relaxed).is_ok() {
+        return;
+    }
+
+    while state.swap(2, Acquire) != 0 {
+        wait(state, 2);
+    }
+}
+
+pub struct MutexGuard<'a, T> {
+    mutex: &'a Mutex<T>,
+}
+
+impl<T> Deref for MutexGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.mutex.value.get() }
+    }
+}
+
+impl<T> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.mutex.value.get() }
+    }
+}
+
+impl<T> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        if self.mutex.state.swap(0, Release) == 2 {
+            wake_one(&self.mutex.state);
+        }
+    }
+}
 
 pub struct Condvar {
     counter: AtomicU32,
