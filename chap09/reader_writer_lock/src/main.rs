2b2a442830437b50e9b9012577958f2db6512ce0
kdnakt <a.kid.1985@gmail.com>
2023-08-03 15:59:56 +0900

    add RwLock::new()

--- chap09/reader_writer_lock/src/main.rs
+++ chap09/reader_writer_lock/src/main.rs
@@ -10,6 +10,15 @@ pub struct RwLock<T> {
 unsafe impl<T> Sync for RwLock<T>
     where T: Send + Sync {}
 
+impl<T> RwLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0), // Unlocked.
+            value: UnsafeCell::new(value),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
