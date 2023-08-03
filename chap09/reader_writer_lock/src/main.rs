cfdbf90caae1e44a839ced0e3e819a0f334928ae
kdnakt <a.kid.1985@gmail.com>
2023-08-03 15:53:41 +0900

    start with a RwLock struct

---
+++ chap09/reader_writer_lock/src/main.rs
@@ -0,0 +1,12 @@
+use std::sync::atomic::AtomicU32;
+use std::cell::UnsafeCell;
+
+pub struct RwLock<T> {
+    /// The number of readers, or u32::MAX if write-locked.
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
