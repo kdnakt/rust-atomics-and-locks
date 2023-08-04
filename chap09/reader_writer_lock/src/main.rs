665e4bd00caca297ada08c32a52c66c710349273
kdnakt <a.kid.1985@gmail.com>
2023-08-04 09:55:39 +0900

    implement Deref and DerefMut for WriteGuard

--- chap09/reader_writer_lock/src/main.rs
+++ chap09/reader_writer_lock/src/main.rs
@@ -1,5 +1,9 @@
 use std::sync::atomic::AtomicU32;
 use std::cell::UnsafeCell;
+use std::ops::{
+    Deref,
+    DerefMut,
+};
 
 pub struct RwLock<T> {
     /// The number of readers, or u32::MAX if write-locked.
@@ -25,6 +29,19 @@ pub struct ReadGuard<'a, T> {
 
 pub struct WriteGuard<'a, T> {
     rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for WriteGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> DerefMut for WriteGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.rwlock.value.get() }
+    }
 }
 
 fn main() {
