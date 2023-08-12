0b83ac5f84c2c68704fc80977587dcccc5324ea0
kdnakt <a.kid.1985@gmail.com>
2023-08-12 13:18:52 +0900

    add ReadGuard and WriteGuard

--- chap09/avoiding_writer_starvation/src/main.rs
+++ chap09/avoiding_writer_starvation/src/main.rs
@@ -14,6 +14,97 @@ pub struct RwLock<T> {
     value: UnsafeCell<T>,
 }
 
+impl<T> RwLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0),
+            writer_wake_counter: AtomicU32::new(0), // New!
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn read(&self) -> ReadGuard<T> {
+        let mut s = self.state.load(Relaxed);
+        loop {
+            if s < u32::MAX {
+                assert!(s < u32::MAX - 1, "too many readers");
+                match self.state.compare_exchange_weak(
+                    s, s + 1, Acquire, Relaxed
+                ) {
+                    Ok(_) => return ReadGuard { rwlock: self },
+                    Err(e) => s = e,
+                }
+            }
+            if s == u32::MAX {
+                wait(&self.state, u32::MAX);
+                s = self.state.load(Relaxed);
+            }
+        }
+    }
+
+    pub fn write(&self) -> WriteGuard<T> {
+        while self.state.compare_exchange(
+            0, u32::MAX, Acquire, Relaxed
+        ).is_err() {
+            let w = self.writer_wake_counter.load(Acquire);
+            if self.state.load(Relaxed) != 0 {
+                // Wait if the RwLock is still locked,
+                // but only if there have been no wake signals since we checked.
+                wait(&self.writer_wake_counter, w);
+            }
+        }
+        WriteGuard { rwlock: self }
+    }
+}
+
+pub struct ReadGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for ReadGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> Drop for ReadGuard<'_, T> {
+    fn drop(&mut self) {
+        if self.rwlock.state.fetch_sub(1, Release) == 1 {
+            self.rwlock.writer_wake_counter.fetch_add(1, Release); // New!
+            wake_one(&self.rwlock.writer_wake_counter); // Changed!
+        }
+    }
+}
+
+pub struct WriteGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for WriteGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> DerefMut for WriteGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.rwlock.value.get() }
+    }
+}
+
+impl<T> Drop for WriteGuard<'_, T> {
+    fn drop(&mut self) {
+        self.rwlock.state.store(0, Release);
+        self.rwlock.writer_wake_counter.fetch_add(1, Release); //New!
+        // wake one waiting writer
+        wake_one(&self.rwlock.writer_wake_counter); // New!
+        // Wake up all waiting readers.
+        wake_all(&self.rwlock.state);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
